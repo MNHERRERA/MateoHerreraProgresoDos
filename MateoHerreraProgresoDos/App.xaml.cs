f754fa23f5c827149d3d5497ec71b087a17ad401
laboratorio <laboratorio@UPOAULA10707>
2025-06-03 07:33:36 -0500

    Agregar archivos de proyecto.

---
+++ MateoHerreraProgresoDos/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MateoHerreraProgresoDos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
